bc4794b848df232ae9e6197ae1ddc50fa8401b9f
Diego Toro Reyes <dtoro.a.s@gmail.com>
2024-08-23 12:01:00 -0400

    menor change env files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    "^.+\\.ts$": "ts-jest",
   },
-  testMatch: ['**/tests/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testMatch: ["**/tests/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  moduleNameMapper: {
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+    "^@core/(.*)$": "<rootDir>/src/core/$1",
+    "^@config/(.*)$": "<rootDir>/src/config/$1",
+    "^@decorators/(.*)$": "<rootDir>/src/decorators/$1",
+    "^@swagger/(.*)$": "<rootDir>/src/swagger/$1",
+    "^@interfaces/(.*)$": "<rootDir>/src/interfaces/$1",
+  },
 };
